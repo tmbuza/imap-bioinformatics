205d1ecc8e4d1a0f49d605ec39da0d3dd201de9e
tmbuza <ndelly@gmail.com>
2023-01-30 17:58:54 -0600

    Improved workflow

--- workflow/scripts/make_files.sh
+++ workflow/scripts/make_files.sh
@@ -4,13 +4,9 @@
 SAMPLEDIR="data/mothur/raw"
 OUTDIR="data/mothur/process"
 LOGS="data/mothur/logs"
-METADIR="data/mothur/metadata"
 
-echo PROGRESS: Assembling the paired-end and screening unique sequences.
+echo PROGRESS: Making mothur-based sample mapping file.
 
-mkdir -p "${OUTDIR}"  "${LOGS}" "${METADIR}"
-# Making contigs from fastq.gz files, aligning reads to references, removing any non-bacterial sequences, calculating distance matrix, making shared file, and classifying OTUs
+mkdir -p "${OUTDIR}"  "${LOGS}"
 mothur "#set.logfile(name=${LOGS}/make_files.logfile);
-      make.file(type=fastq, inputdir="${SAMPLEDIR}", outputdir="${OUTDIR}", prefix=test);"
- 
-# cp ${OUTDIR}/test.files ${METADIR}/sample.files
+      make.file(type=gz, inputdir="${SAMPLEDIR}", outputdir="${OUTDIR}", prefix=test);"
--- workflow/scripts/mothurShared.sh
+++ workflow/scripts/mothurShared.sh
@@ -1,73 +1,39 @@
-#! /bin/bash
-# mothurShared.sh
-# William L. Close
-# Schloss Lab
-# University of Michigan
-
-##################
-# Set Script Env #
-##################
-
-# # Set the variables to be used in this script
-# SAMPLEDIR=${1:?ERROR: Need to define SAMPLEDIR.}
-# SILVAALIGN=${2:?ERROR: Need to define SILVAALIGN.}
-# CLASSIFIER=${3:?ERROR: Need to define CLASSIFIER.}
-# TAXONOMY=${4:?ERROR: Need to define TAXONOMY.}
+#!/usr/bin/env bash
 
 # Set the variables to be used in this script
+OUTDIR=data/mothur/process
+ERRORDIR=${OUTDIR}/error_analysis
+LOGS=${OUTDIR}/logs
+
 SAMPLEDIR="data/mothur/raw"
-# SILVAALIGN="data/mothur/references/silva.v4.align"
 SILVAALIGN="data/mothur/references/silva.v4.align"
 CLASSIFIER="data/mothur/references/trainset16_022016.pds.fasta"
 TAXONOMY="data/mothur/references/trainset16_022016.pds.tax"
 
-# Other variables
+DATASET="test"
 OUTDIR="data/mothur/process"
+FILES="${OUTDIR}"/test.files
+
 LOGS="data/mothur/logs"
 
-# Other variables
 
+mkdir -p "${OUTDIR}" "${LOGS}" "${ERRORDIR}"
 
-
-
-###################
-# Run QC Analysis #
-###################
-
+###############################
 echo PROGRESS: Assembling, quality controlling, clustering, and classifying sequences.
+###############################
 
 # Making output dir
-mkdir -p "${OUTDIR}" "${LOGS}" "${ERRORDIR}" 
+mkdir -p "${OUTDIR}" "${ERRORDIR}" "${LOGS}"
 
 # Making contigs from fastq.gz files, aligning reads to references, removing any non-bacterial sequences, calculating distance matrix, making shared file, and classifying OTUs
-# mothur "#make.file(type=fastq, prefix="test", inputdir="${SAMPLEDIR}", outputdir="${OUTDIR}");
-# 	make.contigs(file=current);
-# 	screen.seqs(fasta=current, group=current, maxambig=0, maxlength=275, maxhomop=8);
-# 	unique.seqs(fasta=current);
-# 	unique.seqs(count=current);
-# 	count.seqs(count=current, group=current);
-# 	align.seqs(fasta=current, reference="${SILVAALIGN}");
-# 	screen.seqs(fasta=current, count=current, start=1968, end=11550);
-# 	filter.seqs(fasta=current, vertical=T, trump=.);
-# 	unique.seqs(fasta=current, count=current);
-# 	pre.cluster(fasta=current, count=current, diffs=2);
-# 	chimera.vsearch(fasta=current, count=current, dereplicate=T);
-# 	remove.seqs(fasta=current, accnos=current);
-# 	classify.seqs(fasta=current, count=current, reference="${CLASSIFIER}", taxonomy="${TAXONOMY}", cutoff=80);
-# 	remove.lineage(fasta=current, count=current, taxonomy=current, taxon=Chloroplast-Mitochondria-unknown-Archaea-Eukaryota);
-# 	dist.seqs(fasta=current, cutoff=0.03);
-# 	cluster(column=current, count=current);
-# 	make.shared(list=current, count=current, label=0.03);
-# 	classify.otu(list=current, count=current, taxonomy=current, label=0.03);
-# 	get.oturep(fasta=current, count=current, list=current, label=0.03, method=abundance)"
-
-mothur "#set.logfile(name=${LOGS}/sample_files.logfile);
+mothur "#set.logfile(name=${LOGS}/make_files.logfile);
 	set.dir(input=${SAMPLEDIR}, output=${OUTDIR});
 	
-	make.file(type=fastq, prefix=test, inputdir=${SAMPLEDIR}, outputdir=${OUTDIR});
+	make.file(type=gz, inputdir=${SAMPLEDIR}, outputdir=${OUTDIR}, prefix=test);
 	
 	set.logfile(name=${LOGS}/seq_assembly.logfile);
-	set.current(file=test.files);
+	set.current(file="${FILES}");
 	make.contigs(file=current);
 	screen.seqs(fasta=current, group=current, maxambig=0, maxlength=275, maxhomop=8);
 	unique.seqs(count=current);
@@ -109,18 +75,22 @@ mothur "#set.logfile(name=${LOGS}/sample_files.logfile);
 
 
 # Renaming output files for use later
-# mv ${OUTDIR}/*.precluster*pick.fasta ${ERRORDIR}/errorinput.fasta
-# mv ${OUTDIR}/*.vsearch*.pick.count_table ${ERRORDIR}"/errorinput.count_table
-mv "${OUTDIR}"/*.opti_mcc.list "${OUTDIR}"/final.list
-mv "${OUTDIR}"/*.opti_mcc.steps "${OUTDIR}"/final.steps
-mv "${OUTDIR}"/*.opti_mcc.sensspec "${OUTDIR}"/final.sensspec
-mv "${OUTDIR}"/*.opti_mcc.shared "${OUTDIR}"/final.shared
-mv "${OUTDIR}"/*.0.03.cons.taxonomy "${OUTDIR}"/final.taxonomy
-mv "${OUTDIR}"/*.0.03.cons.tax.summary "${OUTDIR}"/final.tax.summary
-mv "${OUTDIR}"/*.0.03.rep.fasta "${OUTDIR}"/final.rep.fasta
-mv "${OUTDIR}"/*.0.03.rep.count_table "${OUTDIR}"/final.rep.count_table
-mv "${OUTDIR}"/*.0.03.lefse "${OUTDIR}"/final.lefse
-mv "${OUTDIR}"/*.0.03.biom "${OUTDIR}"/final.biom
+# For error analysis
+cp "${OUTDIR}"/*.vsearch.pick.pick.fasta "${ERRORDIR}"/errorinput.fasta
+cp "${OUTDIR}"/*.vsearch.pick.count_table "${ERRORDIR}"/errorinput.count_table
+
+# For downstream OTU analysis
+cp "${OUTDIR}"/*.opti_mcc.list "${OUTDIR}"/final.list
+cp "${OUTDIR}"/*.opti_mcc.steps "${OUTDIR}"/final.steps
+cp "${OUTDIR}"/*.opti_mcc.sensspec "${OUTDIR}"/final.sensspec
+cp "${OUTDIR}"/*.opti_mcc.shared "${OUTDIR}"/final.shared
+cp "${OUTDIR}"/*.0.03.cons.taxonomy "${OUTDIR}"/final.taxonomy
+cp "${OUTDIR}"/*.0.03.cons.tax.summary "${OUTDIR}"/final.tax.summary
+cp "${OUTDIR}"/*.0.03.rep.fasta "${OUTDIR}"/final.rep.fasta
+cp "${OUTDIR}"/*.0.03.rep.count_table "${OUTDIR}"/final.rep.count_table
+cp "${OUTDIR}"/*.0.03.lefse "${OUTDIR}"/final.lefse
+cp "${OUTDIR}"/*.0.03.biom "${OUTDIR}"/final.biom
+
 
 
 ###############
@@ -132,12 +102,10 @@ echo PROGRESS: Cleaning up working directory.
 # Making dir for storing intermediate files (can be deleted later)
 mkdir -p "${OUTDIR}"/intermediate/
 
-# # # Deleting unneccessary files
-# rm "${OUTDIR}"/*filter.unique.precluster*fasta
-rm "${OUTDIR}"/*.map
-# rm "${OUTDIR}"/*filter.unique.precluster*count_table
+# Deleting unneccessary files
+rm "${OUTDIR}"/*filter.unique.precluster*fasta
+rm "${OUTDIR}"/*filter.unique.precluster*map
+rm "${OUTDIR}"/*filter.unique.precluster*count_table
 
 # Moving all remaining intermediate files to the intermediate dir
 mv "${OUTDIR}"/test* "${OUTDIR}"/intermediate/
-
-mv "${OUTDIR}"/current_files.summary ${LOGS}/current_saved_files.logfile
