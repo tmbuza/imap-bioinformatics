d8adcfb8da6a42cbe46ec7e6aa8584057113f173
tmbuza <ndelly@gmail.com>
2023-02-01 02:11:52 -0600

    Update files

--- workflow/scripts/make_files.sh
+++ workflow/scripts/make_files.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # Creating basic variables
-SAMPLEDIR="data/mothur/raw"
+# SAMPLEDIR="data/mothur/raw"
+SAMPLEDIR="/Volumes/SeagateTMB/MOTHUR_BUSHMEAT/SRR7450/data/reads/"
 OUTDIR="data/mothur/process"
 LOGS="data/mothur/logs"
 
@@ -9,4 +10,4 @@ echo PROGRESS: Making mothur-based sample mapping file.
 
 mkdir -p "${OUTDIR}"  "${LOGS}"
 mothur "#set.logfile(name=${LOGS}/make_files.logfile);
-      make.file(type=gz, inputdir="${SAMPLEDIR}", outputdir="${OUTDIR}", prefix=test);"
+      make.file(type=gz, inputdir="${SAMPLEDIR}", outputdir="${OUTDIR}", prefix=bush);"
