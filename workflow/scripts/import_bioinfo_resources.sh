0ff62f1bce819ebcba59c6a756586514bf990eee
tmbuza <ndelly@gmail.com>
2023-02-15 10:13:16 -0600

    Review and retest IMAP-PART2

--- workflow/scripts/import_bioinfo_resources.sh
+++ workflow/scripts/import_bioinfo_resources.sh
@@ -3,11 +3,11 @@
 # Creating basic variables
 INPUTDIR="../imap-requirements/resources"
 OUTDIR="data"
-# MAPPING="data/metadata"
-# READSDIR="data/reads"
-# TESTDIR="data/test"
-# MOTHURREFS="data/references"
-# MOCKCONTROL="data/mock_control"
+MAPPING="data/metadata"
+READSDIR="data/reads"
+TESTDIR="data/test"
+MOTHURREFS="data/references"
+MOCKCONTROL="data/mock_control"
 
 ###############################
 echo PROGRESS: "Preparing resouces for Bioinformatics (IMAP-PART 2)"
@@ -16,6 +16,9 @@ echo PROGRESS: "Preparing resouces for Bioinformatics (IMAP-PART 2)"
 mkdir -p "${OUTDIR}"
 
 cp -r "${INPUTDIR}"/* "${OUTDIR}"/
+cp "${MOCKCONTROL}"/* "${TESTDIR}"
+
+
 # cp data/metadata/metadata.csv "${MAPPING}"/
 # cp data/metadata/mothur_mapping.tsv "${MAPPING}"/
 # cp data/metadata/mothur_design.tsv "${MAPPING}"/
