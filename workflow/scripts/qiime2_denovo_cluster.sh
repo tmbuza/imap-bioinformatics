13cfa817db75384944dd9f6acddf6f17a0aa97d4
tmbuza <ndelly@gmail.com>
2023-03-24 15:13:17 -0500

    Add all three qiime clustering methods

--- workflow/scripts/qiime2_denovo_cluster.sh
+++ workflow/scripts/qiime2_denovo_cluster.sh
@@ -1,4 +1,4 @@
-echo PROGRESS: Performing denovo clustering.
+echo PROGRESS: Performing denovo clustering with 99% perc-identity.
 
 qiime vsearch cluster-features-de-novo \
   --i-table $PWD/qiime2_process/feature-table.qza \
@@ -7,8 +7,9 @@ qiime vsearch cluster-features-de-novo \
   --o-clustered-table $PWD/qiime2_process/feature-table-dn-99.qza \
   --o-clustered-sequences $PWD/qiime2_process/rep-seqs-dn-99.qza
 
-qiime feature-table tabulate-seqs \
-  --i-data $PWD/qiime2_process/rep-seqs-dn-99.qza \
-  --o-visualization $PWD/qiime2_process/rep-seqs-dn-99.qzv
+# qiime feature-table tabulate-seqs \
+#   --i-data $PWD/qiime2_process/rep-seqs-dn-99.qza \
+#   --o-visualization $PWD/qiime2_process/rep-seqs-dn-99.qzv
 
-# qiime tools view $PWD/qiime2_process/rep-seqs-dn-99.qzv
+# # qiime tools view $PWD/qiime2_process/rep-seqs-dn-99.qzv
+
